04c2d88bc08adb252a44e15375a1d1f62366ed1a
James Haville <>
2018-04-15 19:49:07 +0100

    initial app and gitignore

---
+++ VeganApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  VeganApp
+//
+//  Created by James Haville on 15/04/2018.
+//  Copyright © 2018 Ten Labs Ltd. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
