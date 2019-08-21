d913315b20b0d0947bca4dd7d715770365858555
jhaville <jhaville@users.noreply.github.com>
2019-08-21 13:47:48 +0100

    Update AppDelegate.swift

--- WellnessApp/AppDelegate.swift
+++ WellnessApp/AppDelegate.swift
@@ -11,7 +11,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
   
   func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
     
-    GMSServices.provideAPIKey("AIzaSyBkHiGTpLXHsluDgyPqeWRPIwO7jPNJ33I")
+    GMSServices.provideAPIKey("")
     Fabric.with([Crashlytics.self])
     
     styleApp()
